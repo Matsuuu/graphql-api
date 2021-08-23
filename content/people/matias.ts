953a4e5c92e00935221190c5ac6ae30e195f856b
Matsu <huhta.matias@gmail.com>
2021-08-23 16:03:11 +0300

    Update Matias Huhta profile

--- content/people/matias.ts
+++ content/people/matias.ts
@@ -3,7 +3,7 @@ import Keyword from "../../server/schema/keywords";
 
 const person: Contact = {
   name: "Matias Huhta",
-  about: "",
+  about: "Matias is a Web Components enthusiast actively working on framework agnostic Open Source JavaScript tooling.",
   image: {
     url: "people/matias.jpeg",
   },
@@ -14,7 +14,7 @@ const person: Contact = {
     facebook: "",
     medium: "",
     instagram: "",
-    linkedin: "",
+    linkedin: "https://www.linkedin.com/in/matias-huhta-b0b159106/",
     youtube: "",
     devto: "",
   },
@@ -23,9 +23,9 @@ const person: Contact = {
       name: "Finland",
       code: "FI",
     },
-    city: "Helsinki",
+    city: "Turku",
   },
-  keywords: [Keyword.REACT],
+  keywords: [Keyword.REACT, Keyword.TOOLING],
   type: [ContactType.SPEAKER],
 };
 
